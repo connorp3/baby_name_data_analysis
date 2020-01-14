bde51038363ee64c9fdc395ac59b8bb16b812649
Connor Penny <cgp19@duke.edu>
2020-01-13 19:40:51 -0500

    Separated Main into its own class

--- src/names/BabyData.java
+++ src/names/BabyData.java
@@ -61,21 +61,4 @@ public class BabyData {
         }
         return yearToRank;
     }
-    public static void main (String[] args) {
-        BabyData babyData = new BabyData();
-        babyData.setFileList("C:\\Users\\conno\\Documents\\CS307\\data_cgp19\\data\\ssa_complete");
-
-        System.out.println("Enter a name: ");
-        Scanner scanner = new Scanner(System.in);
-        String name = scanner.nextLine();
-
-        System.out.println("Enter a gender: ");
-        String gender = scanner.nextLine();
-
-        HashMap<Integer, Integer> yearlyNameRankMap = babyData.yearlyNameRank(name, gender);
-
-        for (HashMap.Entry<Integer,Integer> entry : yearlyNameRankMap.entrySet()) {
-            System.out.println(entry.getKey() + ": " + entry.getValue());
-        }
-    }
 }
--- src/names/Main.java
+++ src/names/Main.java
@@ -1,14 +1,25 @@
 package names;
 
+import java.util.HashMap;
+import java.util.Scanner;
 
-/**
- * Feel free to completely change this code or delete it entirely. 
- */
 public class Main {
-    /**
-     * Start of the program.
-     */
+
     public static void main (String[] args) {
-        System.out.println("Hello world");
+        BabyData babyData = new BabyData();
+        babyData.setFileList("C:\\Users\\conno\\Documents\\CS307\\data_cgp19\\data\\ssa_complete");
+
+        System.out.println("Enter a name: ");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+
+        System.out.println("Enter a gender: ");
+        String gender = scanner.nextLine();
+
+        HashMap<Integer, Integer> yearlyNameRankMap = babyData.yearlyNameRank(name, gender);
+
+        for (HashMap.Entry<Integer,Integer> entry : yearlyNameRankMap.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
     }
 }
